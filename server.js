844b59dd5031cbfc39b3cb23931eac84ba4deedc
Yang Zhang <>
2018-06-23 08:24:08 -0700

    Add files.

---
+++ server.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const express = require('express');
+
+const app = express();
+app.use(express.static('static'));
+
+app.listen(3000, function () {
+    console.log('App started on port 3000');
+});
