97d4db007e4647c0ea59ef7e728c00dc2f228039
Yang Zhang <>
2018-07-26 07:09:11 -0700

    update

--- server/server.js
+++ server/server.js
@@ -1,55 +1,59 @@
 'use strict';
 
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
-const MongoClient = require('mongodb').MongoClient;
-const Issue = require('./issue');
+// const MongoClient = require('mongodb').MongoClient;
+// const Issue = require('./issue');
 
 const app = express();
-app.use(express.static('static'));
-app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '/../static')));
+// app.use(bodyParser.json());
 
-
-
-app.get('/api/issues', (req, res) => {
-    db.collection('issues').find().toArray().then(issues => {
-        const metadata = {total_count: issues.length};
-        res.json({_metadata: metadata, records: issues});
-    }).catch(error => {
-        console.log(error);
-        res.status(500).json({message: `Internal Server Error: ${error}`});
-    });
+app.listen(3000, function () {
+    console.log('App started on port 3000');
 });
 
-app.post('/api/issues', (req, res) => {
-    const newIssue = req.body;
-    newIssue.created = new Date();
-    if (!newIssue.status) {
-        newIssue.status = 'New';
-    }
 
-    const err = Issue.validateIssue(newIssue);
-    if (err) {
-        res.status(422).json({message: `Invalid request: ${err}`});
-        return;
-    }
+// app.get('/api/issues', (req, res) => {
+//     db.collection('issues').find().toArray().then(issues => {
+//         const metadata = {total_count: issues.length};
+//         res.json({_metadata: metadata, records: issues});
+//     }).catch(error => {
+//         console.log(error);
+//         res.status(500).json({message: `Internal Server Error: ${error}`});
+//     });
+// });
 
-    db.collection('issues').insertOne(newIssue).then(result =>
-        db.collection('issues').find({_id: result.insertedId}).limit(1).next()
-    ).then(newIssue => {
-        res.json(newIssue);
-    }).catch(error => {
-        console.log(error);
-        res.status(500).json({message: `Internal Server Error: ${error}`});
-    });
-});
+// app.post('/api/issues', (req, res) => {
+//     const newIssue = req.body;
+//     newIssue.created = new Date();
+//     if (!newIssue.status) {
+//         newIssue.status = 'New';
+//     }
+//
+//     const err = Issue.validateIssue(newIssue);
+//     if (err) {
+//         res.status(422).json({message: `Invalid request: ${err}`});
+//         return;
+//     }
+//
+//     db.collection('issues').insertOne(newIssue).then(result =>
+//         db.collection('issues').find({_id: result.insertedId}).limit(1).next()
+//     ).then(newIssue => {
+//         res.json(newIssue);
+//     }).catch(error => {
+//         console.log(error);
+//         res.status(500).json({message: `Internal Server Error: ${error}`});
+//     });
+// });
 
-let db;
-MongoClient.connect('mongodb://localhost/issuetracker').then(connection => {
-    db = connection;
-    app.listen(3000, () => {
-        console.log('App started on port 3000');
-    })
-}).catch(error => {
-    console.log('ERROR: ', error);
-});
+// let db;
+// MongoClient.connect('mongodb://localhost/issuetracker').then(connection => {
+//     db = connection;
+//     app.listen(3000, () => {
+//         console.log('App started on port 3000');
+//     })
+// }).catch(error => {
+//     console.log('ERROR: ', error);
+// });
